696c56cbe5cd484e304970f0093a822a14c7a81d
Vlad-Vasinev <80538465+Vlad-Vasinev@users.noreply.github.com>
2023-05-21 01:02:44 +0300

    Add files via upload

--- src/components/leaflet/Leaflet.js
+++ src/components/leaflet/Leaflet.js
@@ -1,17 +1,12 @@
-import React, { useState } from "react";
-import { Marker, Popup, useMap, MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState, useCallback, useRef, useMemo } from "react";
+import { useEventHandlers } from '@react-leaflet/core'
+import { Marker, Popup, useMap, MapContainer, TileLayer, Polygon, useMapEvent, Rectangle } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import ChooseMap from "../chooseMap/ChooseMap";
 import PositionModal from "../positionModal/PositionModal";
 
 const LeafletMap = () => {
-
-    //https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-    // https://c.tile.openstreetmap.fr/osmfr/10/301/385.png;
-    //https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/10/300/384.png
-    //https://tiles.stadiamaps.com/tiles/osm_bright/8/75/95.png
-    //https://a.tile.thunderforest.com/outdoors/9/151/191.png
 
     const baseMaps = {
         main: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -41,22 +36,140 @@ const LeafletMap = () => {
         shadowUrl: require('leaflet/dist/images/marker-shadow.png')
     });
 
-    return (
-        <MapContainer style={{ height: "500px", width: "100%", position: "relative" }}
-        center={position}
-        zoom={mapZoom}
-        scrollWheelZoom={false}>
-            <TileLayer
-                // attribution='&copy; <a href="https://www.openstreetmap.org/#map=11/40.7811/-74.0630">OpenStreetMap</a> contributors'
-                url={baseMaps[stateMap]}
-            />
-            <ChooseMap stateMap = {setStateMap}></ChooseMap>
-            <PositionModal mapPosition = {setMapPosition} mapZoom = {mapZoomState} ></PositionModal>
-            <Marker position={position}>
-                <Popup>
-                    pretty Manhattan :D <br /> big city :DD
+    function DraggableMarker(props) {
+        const [draggable, setDraggable] = useState(false);
+        const [position, setPosition] = useState([props.lat, props.lang]);
+        const markerRef = useRef(null);
+        const eventHandlers = useMemo(
+            () => ({
+                dragend() {
+                    const marker = markerRef.current
+                    if (marker != null) {
+                        setPosition(marker.getLatLng())
+                    }
+                },
+            }),
+            [],
+        )
+        const toggleDraggable = useCallback(() => {
+            setDraggable((d) => !d)
+        }, [])
+
+        return (
+            <Marker
+                draggable={draggable}
+                eventHandlers={eventHandlers}
+                position={position}
+                ref={markerRef}>
+                <Popup minWidth={90}>
+                    <span onClick={toggleDraggable}>
+                        {draggable
+                            ? props.text_1 + ' ' + "draggable click to disable drag"
+                            : props.text_2 + ' ' + 'not draggable click here to drag it'}
+                    </span>
                 </Popup>
             </Marker>
+        )
+    }
+
+    const minePoly = [
+        [40.7720, -74.0690],
+        [40.7920, -74.0880],
+        [40.7820, -74.0988],
+        [40.7820, -74.1200],
+    ]
+    const colorOption = { color: "orange" };
+
+    const POSITION_CLASSES = {
+        bottomleft: 'leaflet-bottom leaflet-left',
+        bottomright: 'leaflet-bottom leaflet-right',
+        topleft: 'leaflet-top leaflet-left',
+        topright: 'leaflet-top leaflet-right',
+    }
+
+    const BOUNDS_STYLE = { weight: 1 }
+
+    function MinimapBounds({ parentMap, zoom }) {
+        const minimap = useMap()
+
+        const onClick = useCallback(
+            (e) => {
+                parentMap.setView(e.latlng, parentMap.getZoom())
+            },
+            [parentMap],
+        )
+        useMapEvent('click', onClick)
+
+        const [bounds, setBounds] = useState(parentMap.getBounds())
+        const onChange = useCallback(() => {
+            setBounds(parentMap.getBounds())
+            minimap.setView(parentMap.getCenter(), zoom)
+        }, [minimap, parentMap, zoom])
+
+        const handlers = useMemo(() => ({ move: onChange, zoom: onChange }), [])
+        useEventHandlers({ instance: parentMap }, handlers)
+
+        return <Rectangle bounds={bounds} pathOptions={BOUNDS_STYLE} />
+    }
+
+    function MinimapControl({ position, zoom }) {
+        const parentMap = useMap()
+        const mapZoom = zoom || 0
+
+        const minimap = useMemo(
+            () => (
+                <MapContainer
+                    style={{ height: 80, width: 80 }}
+                    center={parentMap.getCenter()}
+                    zoom={mapZoom}
+                    dragging={false}
+                    doubleClickZoom={false}
+                    scrollWheelZoom={false}
+                    attributionControl={false}
+                    zoomControl={false}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <MinimapBounds parentMap={parentMap} zoom={mapZoom} />
+                </MapContainer>
+            ),
+            [],
+        )
+
+        const positionClass =
+            (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright
+        return (
+            <div className={positionClass}>
+                <div className="leaflet-control leaflet-bar">{minimap}</div>
+            </div>
+        )
+    }
+
+    return (
+        <MapContainer style={{ height: "500px", width: "100%", position: "relative" }}
+            center={position}
+            zoom={mapZoom}
+            scrollWheelZoom={false}>
+            <TileLayer
+                url={baseMaps[stateMap]}
+            />
+            <ChooseMap stateMap={setStateMap}></ChooseMap>
+            <PositionModal mapPosition={setMapPosition} mapZoom={mapZoomState} ></PositionModal>
+            <Marker position={position}>
+                <Popup>
+                    pretty Manhattan :D <br />big city :DD
+                </Popup>
+            </Marker>
+            <DraggableMarker
+                lat={40.7620}
+                lang={-74.0760}
+                text_1="first marker"
+                text_2="first marker" />
+            <DraggableMarker
+                lat={40.7820}
+                lang={-74.0740}
+                text_1="second marker"
+                text_2="second marker" />
+            <Polygon pathOptions={colorOption} positions={minePoly}></Polygon>
+            <MinimapControl position="bottomleft" />
         </MapContainer>
     )
 }
