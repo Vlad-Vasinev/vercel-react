1d63a7dcce56b8a34f3e6864a4b8219ba49ce1f5
Vlad-Vasinev <80538465+Vlad-Vasinev@users.noreply.github.com>
2023-05-28 23:26:56 +0300

    Add files via upload

--- src/components/breedsPage/BreedsPage.js
+++ src/components/breedsPage/BreedsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import classes from './breedsPage.module.css';
@@ -13,8 +13,19 @@ import { themeLightCreator } from "../../store/themeReducer";
 import CustomLoad from "../customLoad/CustomLoad";
 import LeafletMap from "../leaflet/Leaflet";
 
-import ReactMapGl, { Layer, ScaleControl, NavigationControl, GeolocateControl, FullscreenControl } from "react-map-gl";
+import ReactMapGl,
+{
+    Layer, ScaleControl,
+    NavigationControl, GeolocateControl,
+    FullscreenControl, Marker, Popup, MarkerDragEvent
+} from "react-map-gl";
+import { LngLat } from 'react-map-gl';
+import markerIcon from '../../assets/images/marker-icon.png';
 
+import YorkFirstImg from '../../assets/images/york1.PNG';
+import YorkSecondImg from '../../assets/images/york2.PNG';
+
+// for production
 import mapboxgl from 'mapbox-gl';
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default; // eslint-disable-line
 
@@ -76,38 +87,69 @@ const Breeds = () => {
 
     const parkLayer = {
         'id': 'add-3d-buildings',
-            'source': 'composite',
-                'source-layer': 'building',
-                    'filter': ['==', 'extrude', 'true'],
-                        'type': 'fill-extrusion',
-                            'minzoom': 15,
-                                'paint': {
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
             'fill-extrusion-color': '#aaa',
 
-                // Use an 'interpolate' expression to
-                // add a smooth transition effect to
-                // the buildings as the user zooms in.
-                'fill-extrusion-height': [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    15,
-                    0,
-                    15.05,
-                    ['get', 'height']
-                ],
-                    'fill-extrusion-base': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'min_height']
-                    ],
-                        'fill-extrusion-opacity': 0.6
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            'fill-extrusion-height': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                15,
+                0,
+                15.05,
+                ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                15,
+                0,
+                15.05,
+                ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
         }
     }
+
+    const [firstMarker, setFirstMarker] = useState({
+        longitude: -74.06342915344241,
+        latitude: 40.794616940239536,
+    });
+
+    const [secondMarker, setSecondMarker] = useState({
+        longitude: -73.97399357604988,
+        latitude: 40.78954841007655,
+    });
+
+    // const [popupOpen, setPopupOpen] = useState(false);
+
+    const [showPopupFirst, setShowPopupFirst] = React.useState(true);
+    const [showPopupSecond, setShowPopupSecond] = React.useState(true);
+
+    const onFirstMarkerDrag = useCallback((event) => {
+
+        setFirstMarker({
+            longitude: event.lngLat.lng,
+            latitude: event.lngLat.lat
+        });
+    }, []);
+
+    const onSecondMarkerDrag = useCallback((event) => {
+
+        setSecondMarker({
+            longitude: event.lngLat.lng,
+            latitude: event.lngLat.lat
+        });
+    }, []);
 
     return (
         <div
@@ -169,20 +211,61 @@ const Breeds = () => {
                 <h2 style={{ marginBottom: "20px", marginTop: "20px" }} className="">MapBox map</h2>
                 <ReactMapGl
                     initialViewState={{
-                        longitude: -74.0630, //-75.6903
-                        latitude: 40.7811, //45.4211
-                        zoom: 12
+                        longitude: -74.0630, //-75.6903 -74.0630  -74.0430
+                        latitude: 40.7811, //45.4211    40.7811   40.7611
+                        zoom: 12,
+                        scrollZoom: false,
                     }}
+                    scrollZoom="disable"
                     mapboxAccessToken="pk.eyJ1IjoidmxhZHZhc2luZXYiLCJhIjoiY2xpMzd2MnlhMmV4ajNkbnQ2YjlzY2wxZyJ9.vmimI0yPvHzI48xMTztqJg" //pk.eyJ1IjoidmxhZHZhc2luZXYiLCJhIjoiY2xpMzhvaWMxMGRnZTNlbXZqbDA0aGI0eSJ9.2PHt8q4XjKlD8Es3tt0-_g
                     style={{ width: "100%", height: 500 }}
                     mapStyle="mapbox://styles/mapbox/navigation-night-v1"
-                    >
-                        <Layer {...parkLayer} />
-                        <ScaleControl />
-                        <NavigationControl position="top-left"/>
-                        <GeolocateControl position="top-left"/>
-                        <FullscreenControl position="top-left"/>
-                    </ReactMapGl>
+                >
+                    <Layer {...parkLayer} />
+                    <ScaleControl />
+                    <NavigationControl position="top-left" />
+                    <GeolocateControl position="top-left" />
+                    <FullscreenControl position="top-left" />
+                    <Marker draggable
+                    onClick={() => setShowPopupFirst(true)}
+                    onDrag={onFirstMarkerDrag}
+                    longitude={firstMarker.longitude}
+                    latitude={firstMarker.latitude} anchor="bottom" >
+                        <img src={markerIcon} />
+                    </Marker>
+                    <Marker draggable
+                    onDrag={onSecondMarkerDrag}
+                    longitude={secondMarker.longitude}
+                    latitude={secondMarker.latitude} anchor="bottom" >
+                        <img src={markerIcon} />
+                    </Marker>
+                    {showPopupFirst && (
+                        <Popup longitude={firstMarker.longitude} latitude={firstMarker.latitude}
+                            anchor="top"
+                            closeOnClick="false"
+                            onClose={() => setShowPopupFirst(false)}>
+                            place 1
+                            <br></br>
+                            {firstMarker.longitude}
+                            <br></br>
+                            {firstMarker.latitude}
+                            <br></br>
+                            <img style={{width: "100%", height: "150px"}} src={YorkFirstImg} alt="New York's day"></img>
+                        </Popup>)}
+                    {showPopupSecond && (
+                        <Popup longitude={secondMarker.longitude} latitude={secondMarker.latitude}
+                            anchor="top"
+                            closeOnClick="false"
+                            onClose={() => setShowPopupSecond(false)}>
+                            place 2
+                            <br></br>
+                            {secondMarker.longitude}
+                            <br></br>
+                            {secondMarker.latitude}
+                            <br></br>
+                            <img style={{width: "100%", height: "150px"}} src={YorkSecondImg} alt="New York's night"></img>
+                        </Popup>)}
+                </ReactMapGl>
             </div>
             <div>
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
